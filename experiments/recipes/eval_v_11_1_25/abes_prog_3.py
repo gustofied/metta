0520e793766d39e40455652100f32aa06a89d9b8
Alex Vardakostas <avardakostas@gmail.com>
2025-10-16 08:29:54 -0700

    sp

--- experiments/recipes/eval_v_11_1_25/abes_prog_3.py
+++ experiments/recipes/eval_v_11_1_25/abes_prog_3.py
@@ -48,6 +48,10 @@ def mettagrid(num_agents: int = 24) -> MettaGridConfig:
     if isinstance(altar, ConverterConfig) and hasattr(altar, "input_resources"):
         altar.input_resources["battery_red"] = 1
 
+    arena_env.game.map_builder.width = 40
+    arena_env.game.map_builder.height = 40
+    arena_env.game.actions.attack.enabled = False
+
     return arena_env
 
 
